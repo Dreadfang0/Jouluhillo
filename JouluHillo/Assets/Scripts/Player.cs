6afb5feeb6792f80dddb383bc9dd9b2e4ab4eadd
JoonaJappinen <jappinen.joona@gmail.com>
2018-11-24 19:03:41 +0200

    Say jump to space
    Jumping enabled

--- JouluHillo/Assets/Scripts/Player.cs
+++ JouluHillo/Assets/Scripts/Player.cs
@@ -4,23 +4,40 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+    bool IsGrounded = false;
+    Rigidbody2D rb2D;
+    [SerializeField]
+    float JumpForce;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-		if(Input.GetKey("up"))
+     
+		if(Input.GetKey("space") && IsGrounded == true)
         {
-            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 15));
-            Debug.Log("pressed up");
-        }
-
-        if(Input.GetKey("right"))
-        {
-            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(10,0));
-        }
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, JumpForce));
+            IsGrounded = false;
+        }      
 	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            IsGrounded = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //Debug.DrawLine(new Vector2(transform.position.x, transform.position.y), new Vector2(0,0));
+        //RaycastHit2D GroundTrigger = Physics2D.BoxCast(new Vector2(transform.position.x, transform.position.y), new Vector2(1,1), 0f, new Vector2(0, -2));
+    }
+
 }
