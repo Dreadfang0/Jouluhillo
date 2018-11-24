94ef1b3813282b56d8c464393edfdd906f902c35
JoonaJappinen <jappinen.joona@gmail.com>
2018-11-24 10:29:05 +0200

    ping pong
    Jannelle sprittia

---
+++ JouluHillo/Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetKey("up"))
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 15));
+            Debug.Log("pressed up");
+        }
+
+        if(Input.GetKey("right"))
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(10,0));
+        }
+	}
+}
