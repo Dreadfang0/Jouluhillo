6ae547eb1db9073febef5e2d68061c05cdb5e5cc
JoonaJappinen <jappinen.joona@gmail.com>
2018-11-25 16:27:02 +0200

    Added Mainmenu

---
+++ JouluHillo/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+
+    [SerializeField]
+    Scene Game;
+
+    public Button StartButton;
+    public Button Exit;
+
+    // Use this for initialization
+    void Start ()
+    {
+        StartButton.onClick.AddListener(StartGame);
+        Exit.onClick.AddListener(QuitGame);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Combined");
+        Debug.Log("Started game");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
